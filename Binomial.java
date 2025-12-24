6c365f863d05e3ba0c28ba1f40071c4062605d45
Maya Hasson <maya.hasson@post.runi.ac.il>
2025-12-24 18:11:47 +0200

    task2

--- Binomial.java
+++ Binomial.java
@@ -24,38 +24,25 @@ public class Binomial {
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		//// This function creates a 2D array, say memo, 
-		//// and then initializes all its elements to -1.
-		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
-		//// Replace the following statement with your code.
-		int[][] newA=new int[n+1][k+1];
-		for(int i=0;i<newA.length;i++)
-		{
-			for(int j=0;j<newA[i].length;j++)
-			{
-				newA[i][j]=-1;
-			}
-		}
-		return binomial(n,k,newA);
-	}
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
-			return memo[n][k];
-		}
-		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
-		return memo[n][k];
-	}
+	public static long binomial(int n, int k) {
+    long[][] memo = new long[n+1][k+1];
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= k; j++) {
+            memo[i][j] = -1;
+        }
+    }
+    return binomial(n, k, memo);
+}
+
+private static long binomial(int n, int k, long[][] memo) {
+    if (memo[n][k] != -1) return memo[n][k];
+
+    if (k > n) return memo[n][k] = 0;
+    if (k == 0 || k == n) return memo[n][k] = 1;
+
+    return memo[n][k] = binomial(n-1,k,memo) + binomial(n-1,k-1,memo);
+}
+
 }
 
 
