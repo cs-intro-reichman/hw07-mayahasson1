76cb44b3719207a810ce494db02610c49f702448
Maya Hasson <maya.hasson@post.runi.ac.il>
2025-12-24 18:07:06 +0200

    task1

--- Binomial.java
+++ Binomial.java
@@ -12,8 +12,15 @@ public class Binomial {
 
 	// Computes the Binomial function, basic version.
 	public static int binomial1(int n, int k) { 
-		//// Repplace the following comment with your code
-		return 0;
+		if(k<0||k>n)
+		{
+			return 0;
+		}
+		if(k==0||k==n)
+		{
+			return 1;
+		}
+		return binomial1(n-1,k)+binomial1(n-1,k-1);
 	 }
 	
 	// Computes the Binomial function, efficiently
@@ -22,9 +29,16 @@ public class Binomial {
 		//// and then initializes all its elements to -1.
 		//// It then calls binomial(n, k, memo), which does all the heavy lifiting.
 		//// Replace the following statement with your code.
-		return 0;
+		int[][] newA=new int[n+1][k+1];
+		for(int i=0;i<newA.length;i++)
+		{
+			for(int j=0;j<newA[i].length;j++)
+			{
+				newA[i][j]=-1;
+			}
+		}
+		return binomial(n,k,newA);
 	}
-
 	private static int binomial(int n, int k, int[][] memo) {
 		if (memo[n][k] != -1) {
 			return memo[n][k];
